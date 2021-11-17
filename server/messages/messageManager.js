88db1399e52ae7bd08f34878e4bdbcc35e50d5b1
Eithan Méndez Méndez <emendez@datasoft.co.cr>
2021-11-16 23:51:38 -0600

    Fixes on message and token manager clases

--- server/messages/messageManager.js
+++ server/messages/messageManager.js
@@ -1,6 +1,13 @@
-module.UnauthorizedMessage = function UnauthorizedMessage() {
+exports.UnauthorizedMessage = () => {
     return {
         status: 401,
         response: "Unauthorized request..."
+    };
+};
+
+exports.UnauthorizedCustomMessage = (message) => {
+    return {
+        status: 401,
+        response: message
     };
 };
